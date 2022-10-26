827798dc2d00a2612552cb434a9db7d93940105b
Harold Hernandez <harold.hernandez30b@gmail.com>
2022-10-27 03:22:03 +0800

    Added new weather handling

--- pages/weather/index.tsx
+++ pages/weather/index.tsx
@@ -4,6 +4,8 @@ export default function WeatherPage() {
     return (
         <div>
             <p>Hot weather</p>
+            <p> New feature</p>
+            <h1>Cold weather</h1>
         </div>
     )
 
